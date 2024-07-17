ce49d75bec8358e002192e4fdd60f3f7960228d9
Henrique Custódia <martinshenrique8@gmail.com>
2024-07-17 20:26:34 -0300

    feat: cria projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,80 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe, CurrencyPipe, TitleCasePipe } from "@angular/common";
+import { Component, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { interval } from "rxjs";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AsyncPipe, TitleCasePipe, CurrencyPipe],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <fieldset>
+      @let text = title + " Henrique!";
 
-    <router-outlet />
+      <h1>{{ text }}</h1>
+    </fieldset>
+
+    <fieldset>
+      @let time = interval | async;
+
+      @let notNullableTime = time ?? 0;
+
+      @let seconds = notNullableTime + "s";
+
+      <h1>{{ seconds }}</h1>
+    </fieldset>
+
+    <fieldset>
+      <div #greeting>Heyy</div>
+
+      @let greetingText = greeting.textContent + " Code Dimension!";
+
+      <h1 [innerHTML]="greetingText"></h1>
+    </fieldset>
+
+    <fieldset>
+      <ul>
+        @for (p of products(); track p.id) {
+          @let name = p.name | titlecase;
+          @let price = p.price | currency;
+
+          @let title = name + " - " + price;
+
+          <li>
+            {{ title }}
+
+            @let categories = p.categories;
+            <ul>
+              @for (c of categories; track c) {
+                @let categoryName = c | titlecase;
+
+                <li>{{ categoryName }}</li>
+              }
+            </ul>
+          </li>
+        }
+      </ul>
+    </fieldset>
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'let-syntax';
+  title = "Oi";
+
+  interval = interval(1000);
+
+  products = signal([
+    {
+      id: 1,
+      name: "product 1",
+      price: 10,
+      categories: ["category 1", "category 2"],
+    },
+    {
+      id: 2,
+      name: "product 2",
+      price: 10,
+      categories: ["category 1", "category 2"],
+    },
+  ]);
 }
